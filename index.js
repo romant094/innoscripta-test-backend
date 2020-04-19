b8e56e9f012dc1c753ea2e651c85a6185f23718b
Anton Romankov <ar@askque.ru>
2020-04-19 17:41:08 +0300

    Remove cors support

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
 
 const routes = require('./routes/index.js');
 
@@ -18,8 +17,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
-app.use(cors());
 
 app.use('/api/v1', routes(router));
 
